4fee428a66d045212dec185a1f50a030218c08c9
fusionmaster7 <hardiksingh297@gmail.com>
2023-03-08 23:16:37 +0530

    Single Character Lexical Scanning Finished

---
+++ src/main/java/com/loxxer/lexical/LexicalTokenType.java
@@ -0,0 +1,9 @@
+package com.loxxer.lexical;
+
+public enum LexicalTokenType {
+    // Single character tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, PLUS, MINUS, STAR, SLASH, COMMA, DOT, SEMICOLON,
+
+    // End of file token
+    EOF
+}
